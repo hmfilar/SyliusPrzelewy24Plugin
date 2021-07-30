98bda515305ee75129fb284e6ced6f94c684483c
Hubert Filar <hubert.filar@gmail.com>
2021-07-30 07:59:30 +0200

    Replace deprecated Debug class

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
 use Tests\BitBag\SyliusPrzelewy24Plugin\Application\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
